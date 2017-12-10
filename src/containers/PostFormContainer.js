8fb178eac39f6b70265da345b778ca06b5086e9f
Alex Emelyanov <holyketzer@gmail.com>
2017-12-10 22:54:56 +0300

    Fix props fetching from state in PostFormContainer

--- src/containers/PostFormContainer.js
+++ src/containers/PostFormContainer.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
+import { get } from 'lodash';
 
 import PostForm from 'components/PostForm';
 
 const stateToProps = (state) => ({
   initialValues: {
-    id: state.post.entry.id,
-    text: state.post.entry.text,
-    author: state.post.entry.metadata.author,
-    createdAt: state.post.entry.metadata.createdAt,
+    id: get(state, 'post.entry.id'),
+    text: get(state, 'post.entry.text'),
+    author: get(state, 'post.entry.metadata.author'),
+    createdAt: get(state, 'post.entry.metadata.createdAt'),
   },
   enableReinitialize: true,
 });
